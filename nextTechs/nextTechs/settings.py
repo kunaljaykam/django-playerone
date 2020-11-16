278b9444626da0565ffe6896ef09d5ad5e479cce
kunal jaykam <kunaljaykam@icloud.com>
2020-11-16 21:42:09 +0530

    add django heroku

--- nextTechs/nextTechs/settings.py
+++ nextTechs/nextTechs/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +130,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+django_heroku.settings(locals())
