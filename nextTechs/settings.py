977e99dcabec049adfdb00e076a8afa051c13e11
kunal jaykam <kunaljaykam@icloud.com>
2020-11-16 23:45:22 +0530

    added settings

--- nextTechs/settings.py
+++ nextTechs/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '442f3e51a5d47e95b82f8ff6594b162a1f7987316547b66b'
-
+#SECRET_KEY = '442f3e51a5d47e95b82f8ff6594b162a1f7987316547b66b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
